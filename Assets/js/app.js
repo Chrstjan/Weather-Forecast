c2a1ecc12acc2d95028842f00f01edf21c3c22f7
Christian <ps5christian@hotmail.com>
2024-03-23 16:21:25 +0100

    Base Js structure & code
    Made MVC regions and global code

---
+++ Assets/js/app.js
@@ -0,0 +1,15 @@
+//GLOBALS
+const locationHeader = document.getElementById("location-header");
+const app = document.getElementById("app");
+
+//#region model code
+
+//#endregion model code
+
+//#region controller code
+
+//#endregion model code
+
+//#region view code
+
+//#endregion view code
