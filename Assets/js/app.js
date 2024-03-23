771c49a7c5206a395101727dded06e3c9bb9ee0b
Christian <ps5christian@hotmail.com>
2024-03-23 16:41:43 +0100

    Getting user location

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,12 +2,49 @@
 const locationHeader = document.getElementById("location-header");
 const app = document.getElementById("app");
 
-//#region model code
+//Calling functions
+getUserCoordinates();
 
+//#region model code
+function getUserCoordinates() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      recivedCoordinates,
+      coordinatesError
+    );
+  } else {
+    alert("Geolocation is not supported by this browser");
+  }
+}
+
+function getUserLocation(lat, long) {
+  fetch(
+    `https://geocode.maps.co/reverse?lat=${lat}&lon=${long}&api_key=65fbef1c16355178751609wmp6b195b`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((error) => {
+      console.log("Error fetching data:", error);
+      return null;
+    });
+}
 //#endregion model code
 
 //#region controller code
+function recivedCoordinates(position) {
+  getUserLocation(position.coords.latitude, position.coords.longitude);
+}
 
+function coordinatesError(error) {
+  console.log(error.message);
+}
 //#endregion model code
 
 //#region view code
